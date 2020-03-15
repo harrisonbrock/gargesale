dc9b558fa42878c6ba8e266cb4513427642050c4
Harrison Brock <harrison@harrisonbrock.com>
2020-03-14 20:25:44 -0400

    Refactored database and schema into internal

---
+++ internal/platform/database/database.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // register postgresql
+	"net/url"
+)
+
+func Open() (*sqlx.DB, error) {
+	q := url.Values{}
+	q.Set("sslmode", "disable")
+	q.Set("timezone", "utc")
+
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword("postgres", "postgres"),
+		Host:     "localhost",
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
+}
