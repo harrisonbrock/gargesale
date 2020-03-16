d739162b954a113f0a0adff7f47e4b0cc54bf472
Harrison Brock <harrison@harrisonbrock.com>
2020-03-16 13:53:07 -0400

    Update flags on logger

--- cmd/sales-api/main.go
+++ cmd/sales-api/main.go
@@ -23,7 +23,7 @@ func main() {
 }
 func run() error {
 
-	log := log.New(os.Stdout, "SALES : ", log.LstdFlags)
+	log := log.New(os.Stdout, "SALES : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
 	var cfg struct {
 		Web struct {
