cd14b9af72542c4c4d22f920481ea377521ed909
Harrison Brock <harrison@harrisonbrock.com>
2020-03-17 11:18:07 -0400

    Added new routes

---
+++ cmd/sales-api/internal/handlers/routes.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"github.com/harrisonbrock/gargesale/internal/platform/web"
+	"github.com/jmoiron/sqlx"
+	"log"
+	"net/http"
+)
+
+func API(logger *log.Logger, db *sqlx.DB) http.Handler {
+
+	app := web.NewApp(logger)
+	p := Products{
+		DB:  db,
+		Log: logger,
+	}
+
+	app.Handle(http.MethodGet, "/v1/products", p.List)
+	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
+
+	return app
+}
