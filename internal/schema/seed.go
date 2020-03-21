b5aef4cf9ae541f76de6321c1e374070af4215cc
Harrison Brock <harrison@harrisonbrock.com>
2020-03-20 23:02:44 -0400

    updated date-time to use utc

--- internal/schema/seed.go
+++ internal/schema/seed.go
@@ -19,8 +19,8 @@ import (
 
 const seeds = `
 INSERT INTO products (product_id, name, cost, quantity, date_created, date_updated) VALUES
-	('a2b0639f-2cc6-44b8-b97b-15d69dbb511e', 'Comic Books', 50, 42, '2019-01-01 00:00:01.000001+00', '2019-01-01 00:00:01.000001+00'),
-	('72f8b983-3eb4-48db-9ed0-e45cc6bd716b', 'McDonalds Toys', 75, 120, '2019-01-01 00:00:02.000001+00', '2019-01-01 00:00:02.000001+00')
+	('a2b0639f-2cc6-44b8-b97b-15d69dbb511e', 'Comic Books', 50, 42, '2019-01-01T00:00:01.000001Z', '2019-01-01T00:00:01.000001Z'),
+	('72f8b983-3eb4-48db-9ed0-e45cc6bd716b', 'McDonalds Toys', 75, 120, '2019-01-01T00:00:02.000001Z', '2019-01-01T00:00:02.000001Z')
 	ON CONFLICT DO NOTHING;
 `
 
