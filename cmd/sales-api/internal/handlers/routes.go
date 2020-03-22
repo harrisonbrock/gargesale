1e2ccdac66da5af10d42677bd63420ee1bfbbb04
Harrison Brock <harrison@harrisonbrock.com>
2020-03-21 22:53:28 -0400

    Formatted code.

--- cmd/sales-api/internal/handlers/routes.go
+++ cmd/sales-api/internal/handlers/routes.go
@@ -15,9 +15,14 @@ func API(logger *log.Logger, db *sqlx.DB) http.Handler {
 		Log: logger,
 	}
 
+	// Products.
 	app.Handle(http.MethodGet, "/v1/products", p.List)
 	app.Handle(http.MethodGet, "/v1/products/{id}", p.Retrieve)
 	app.Handle(http.MethodPost, "/v1/products", p.Create)
+
+	// Sales.
+	app.Handle(http.MethodPost, "/v1/products/{id}/sales", p.AddSale)
+	app.Handle(http.MethodGet, "/v1/products/{id}/sales", p.ListSales)
 
 	return app
 }
