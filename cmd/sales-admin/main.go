13163552ef0d39845ce56ffecb14459d66a7dea1
Harrison Brock <harrison@harrisonbrock.com>
2020-04-23 12:53:43 -0400

    Added function to create key

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"github.com/harrisonbrock/gargesale/internal/platform/auth"
 	"github.com/harrisonbrock/gargesale/internal/platform/conf"
@@ -67,6 +71,8 @@ func run() error {
 		err = seed(dbConfig)
 	case "useradd":
 		err = useradd(dbConfig, cfg.Args.Num(1), cfg.Args.Num(2))
+	case "keygen":
+		err = keygen(cfg.Args.Num(1))
 	default:
 		err = errors.New("Must specify a command")
 	}
@@ -147,5 +153,38 @@ func useradd(cfg database.Config, email, password string) error {
 	}
 
 	fmt.Println("User created with id:", u.ID)
+	return nil
+}
+
+// keygen creates an x509 private key for signing auth tokens.
+func keygen(path string) error {
+	if path == "" {
+		return errors.New("keygen missing argument for key path")
+	}
+
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return errors.Wrap(err, "generating keys")
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return errors.Wrap(err, "creating private file")
+	}
+	defer file.Close()
+
+	block := pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+
+	if err := pem.Encode(file, &block); err != nil {
+		return errors.Wrap(err, "encoding to private file")
+	}
+
+	if err := file.Close(); err != nil {
+		return errors.Wrap(err, "closing private file")
+	}
+
 	return nil
 }
